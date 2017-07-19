89304f30c6e9a45c1e11d64993dc6adb857fe8cc
Kymaro <eclipse.florian@gmail.com>
2017-07-19 11:07:17 +0200

    modif code pour local time

--- code/station.py
+++ code/station.py
@@ -3,13 +3,13 @@ from grovepi import *
 from grove_rgb_lcd import *
 import time,datetime,json
 from math import *
-#from azure.servicebus import ServiceBusService
-#from azure.servicebus import Message
+from azure.servicebus import ServiceBusService
+from azure.servicebus import Message
 ###########################################DECLARATION DES CONSTANTES GLOBALES###########################################
 dht_sensor_port = 7 #capteur humidite et temperature (DHT11 ou DHT22) sur D7
 dht_sensor_type = 0 #mettre 0 si bleu (DHT11) ou 1 si blanc (DHT22)
 t_actuator = 799 #nombre de loop entre deux envoie de donnees
-t_wait = 25 
+t_wait = 30
 lum_seuil = 10 #seuil au dessus du quel on determine la lumiere comme allume
 lum_sensor = 1 #capteur de lumiere sur A1
 potentiometer = 2 #bouton de menu sur A2
@@ -20,16 +20,16 @@ compteur_echec_envoie = 0 # compteur d echec d envoie de donnee sur azure (se re
 mode_value = 0 # les deux valeurs permette d eviter une actualisation inutile de l ecran qui le faisait clignoter
 mode_value_old = 0
 ##############################################VARIABLES DE DONNEES ENVOYEES##############################################
-temp_dht = 0
+temp = 0
 hum = 0
 lum = 0
 lum_statut = False
-identifiant = "Identifiant" # a modifier
+identifiant = "Rpitest" # a modifier
 ########################################################FONCTIONS########################################################
 def DHT() : #temperature et humidite numerique
-    global temp_dht,hum
-    [temp_dht,hum] = dht(dht_sensor_port,dht_sensor_type)
-    temp_dht = round(temp_dht,1)
+    global temp,hum
+    [temp,hum] = dht(dht_sensor_port,dht_sensor_type)
+    temp = round(temp,1)
 
 def Luminosite() : #luminosite qui envoie True ou False avec le seuil
     global lum_statut
@@ -50,7 +50,7 @@ def screen_administrator() : # permet de gerer lecran sans quil refresh a chaque
     global mode_value
     mode_value_old = mode_value
     if (encoder_value <=341 and encoder_value >= 0) and mode_value != 1 : #MODE 1
-       	setText("Temperature : \n" +str(temp_dht))
+       	setText("Temperature : \n" +str(temp))
        	setRGB(0,128,255)
        	mode_value = 1
     elif (encoder_value <= 682 and encoder_value > 341) and mode_value != 2 : #MODE 2
@@ -69,17 +69,17 @@ def screen_administrator() : # permet de gerer lecran sans quil refresh a chaque
         time.sleep(140.0/1000.0) #on attend 140 ms pour etre sur du temps a chaque loop
 
 
-#def createSBS() : #permet de creer le canal de communication avec Azure 
+def createSBS() : #permet de creer le canal de communication avec Azure 
 	
-#    service_namespace = 'namespaceName' #a modifier
-#    key_name = 'SharedAccessKeyName' # a modifier
-#    key_value = 'SharedAccessKey' # a modifier
+    service_namespace = 'RaspberryPiNSTest' #a modifier
+    key_name = 'RootManageSharedAccessKey' # a modifier
+    key_value = 'yEcs0927kFYs1U8J7x4VCwZAaf3ck38hqSGY9YjiMAo=' # a modifier
 	
-#    sbs = ServiceBusService(service_namespace, shared_access_key_name=key_name, shared_access_key_value=key_value)
+    sbs = ServiceBusService(service_namespace, shared_access_key_name=key_name, shared_access_key_value=key_value)
 
-#    return sbs
+    return sbs
 ##########################################################SETUP##########################################################
-#sbs = createSBS()
+sbs = createSBS()
 setText("Bienvenue\ndans l'IoT Hub")
 setRGB(128,255,0)
 time.sleep(2)
@@ -90,13 +90,14 @@ while True :
     if ( t_refresh >= t_actuator) :
         Luminosite()
 	DHT()
-	while (isnan(temp_dht) or temp_dht == 0) : # on essaie tant que le capteur n a pas de valeur valide
+	while (isnan(temp) or temp == 0) : # on essaie tant que le capteur n a pas de valeur valide
 	    DHT()
         dt = str(datetime.datetime.now())
-        d = {'DeviceID' : identifiant, 'Temperature' : temp_dht, 'Humidity' : hum,'Time' : dt }
+        d = {'DeviceID' : identifiant,'Time' : "France/Local Time here",'Temperature' : temp, 'Humidity' : hum }
         msg = json.dumps(d) #cree le message a envoyer
+        print(msg)
         try :
-            #sbs.send_event('eventHubName',msg) #a modifier
+            sbs.send_event('dht11',msg) #a modifier
             compteur_echec_envoie = 0
         except :
             compteur_echec_envoie += 1
